f567b82321b524d88981aea499118185f296aa07
Pedro A. Aranda Gutierrez <paaguti@gmail.com>
2022-11-27 07:44:19 +0100

    Integrate pull 1004 to build ofsoftswitch13

--- util/install.sh
+++ util/install.sh
@@ -148,8 +148,9 @@ OVS_BUILDSUFFIX=-ignore # was -2
 OVS_PACKAGE_NAME=ovs-$OVS_RELEASE-core-$DIST_LC-$RELEASE-$ARCH$OVS_BUILDSUFFIX.tar
 OVS_TAG=v$OVS_RELEASE
 
-OF13_SWITCH_REV=${OF13_SWITCH_REV:-""}
-
+#FROM pull 1104
+#OF13_SWITCH_REV=${OF13_SWITCH_REV:-""}
+OF13_SWITCH_REV=${OF13_SWITCH_REV:-"aaebdbce3dae0f3d9af62f059e3ec5a2813469c4"}
 
 function kernel {
     echo "Install Mininet-compatible kernel if necessary"
@@ -307,6 +308,8 @@ function of13 {
     # Resume the install:
     cd $BUILD_DIR/ofsoftswitch13
     ./boot.sh
+    # Pull 1104
+    sed -i 's/^AM_CFLAGS = -Wstrict-prototypes -Werror$/AM_CFLAGS = -Wstrict-prototypes -Werror -Wno-error=stringop-truncation -Wno-error=format-truncation=/g' Makefile.am
     ./configure
     make
     sudo make install
